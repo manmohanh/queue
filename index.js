36ce9ea16a9acff14d3952b22e837a6ae9c697da
Manmohan Hansda <hmanmohan12@gmail.com>
2025-11-30 20:05:15 +0530

    added redis

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ createClient()
     console.log("failed to connect with redis");
   })
   .connect();
+
+import ioRedis from "ioredis"
+import express from "express";
+import { faker } from "@faker-js/faker";
+import { Queue, Worker } from "bullmq";
+
+const app = express();
+const redisConnection = new ioRedis({maxRetriesPerRequest:null})
+app.listen(8080);
+
+app.post("/message", async (req, res) => {
+  try {
+    const payload = {
+      email: faker.internet.email(),
+      message: faker.lorem.paragraph(),
+    };
+    const messageQ = new Queue("messageQ");
+    await messageQ.add("sendMessage", payload);
+    res.json({ message: "Task added to queue" });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+const sendMessage = (job) => {
+  console.log(job.data);
+};
+
+new Worker("messageQ", sendMessage,{connection:redisConnection});
